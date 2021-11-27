289dc0e3fcf9d2ac422bfc24b8e297b3112574a3
Brayan-Perez-Martinez <pbryantsam@gmail.com>
2021-11-27 17:59:23 -0600

    first commit

---
+++ src/config.js
@@ -0,0 +1,10 @@
+import { config } from "dotenv";
+config()
+
+export default {
+    MONGODB_URI: "mongodb+srv://brayan:Mongodb123@cluster0.dsdne.mongodb.net/academia?retryWrites=true&w=majority",
+    PORT:process.env.PORT || 4000,
+    CLOUD_NAME:"dq6ccwero",
+    API_KEY:748117236366886,
+    API_SECRET:"TH0nCXv8Cm8Z2A44IWc85frPNqY",
+}  
